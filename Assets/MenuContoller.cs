33e7a2abc50855ef025a9cbb5fd26ddb6af11fe1
nirajg52 <Nirajgopani89@gmail.com>
2022-04-12 17:01:32 -0400

    Menu Scene

---
+++ Assets/MenuContoller.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuContoller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void DoControlStuff()
+    {
+
+    }
+
+    void DoMainStuff()
+    {
+
+    }
+}
